2b676993e41f1f10e44411c4b7c6509a5b8cbc9d
MaciejDziuban <dziuban.maciej@gmail.com>
2025-08-12 22:06:25 +0200

    Bump version to 0.1.1

--- common/constants.go
+++ common/constants.go
@@ -3,4 +3,4 @@ package common
 import "spieven/common/buildopts"
 
 const HostWithPort = "localhost:" + buildopts.DefaultPort
-const Version = "0.1.0" + buildopts.VersionSuffix
+const Version = "0.1.1" + buildopts.VersionSuffix
