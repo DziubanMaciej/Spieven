912a3eb6c3178d4605851faa09970014e49129d7
MaciejDziuban <dziuban.maciej@gmail.com>
2025-08-12 21:18:08 +0200

    Improve release script

---
+++ release/build_release.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+script_path="$(realpath "${BASH_SOURCE[0]}")"
+script_path="$(dirname "$script_path")"
+
+cd "$script_path" || exit "Failed cd"
+rm spieven* -f
+
+go build -tags "handshake_check,version_release" -ldflags="-s -w" -trimpath .. || exit "Failed to build spieven"
+version="$(./spieven version)" || exit "Failed to check spieven version"
+zipname=spieven-$version.zip
+zip $zipname spieven >/dev/null || exit "Failed to create zip"
+
+echo "Built spieven version $version"
+realpath spieven || exit "Path error"
+realpath $zipname || exit "Path error"
